c97406f527b2245cc9ea1f630af048f65305399f
Feng Shu <tumashu@163.com>
2022-03-12 15:22:17 +0800

    Add https://bordeaux.guix.gnu.org

--- system-config.scm
+++ system-config.scm
@@ -16,7 +16,8 @@
 
 (define %my-substitute-urls
   (list "https://mirror.sjtu.edu.cn/guix/"
-	"https://ci.guix.gnu.org"))
+	"https://ci.guix.gnu.org"
+        "https://bordeaux.guix.gnu.org"))
 
 (operating-system
   (kernel linux/thinkpad-t14-amd)
