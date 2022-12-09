c233019504a45182170db42bb9dc46b2c25333af
Feng Shu <tumashu@163.com>
2022-12-09 20:49:19 +0800

    Use customize-linux

--- gee/packages/linux.scm
+++ gee/packages/linux.scm
@@ -1,24 +1,13 @@
 (define-module (gee packages linux)
   #:use-module (gnu)
+  #:use-module (gnu packages linux)
   #:use-module (guix packages)
-  #:use-module (ice-9 textual-ports)
-  #:use-module (nongnu packages linux)
-  #:use-module (srfi srfi-1))
+  #:use-module (nongnu packages linux))
 
 (define-public linux/thinkpad-t14-amd
-  (let* ((native-inputs (package-native-inputs linux))
-         (orig-config-str
-          (call-with-input-file (car (assoc-ref native-inputs "kconfig"))
-            get-string-all))
-         (config (mixed-text-file
-                  "thinkpad-t14-amd.config"
-                  orig-config-str
-                  "
-# Add by linux-thinkpad-t14-amd.
-CONFIG_MT7921E=m")))
-    (package
-      (inherit linux)
-      (name "linux-thinkpad-t14-amd")
-      (native-inputs
-       `(("kconfig" ,config)
-         ,@(alist-delete "kconfig" native-inputs))))))
+  (customize-linux
+   #:name "linux-thinkpad-t14-amd"
+   #:linux linux
+   #:configs
+   '("# Add by linux-thinkpad-t14-amd."
+     "CONFIG_MT7921E=m")))
