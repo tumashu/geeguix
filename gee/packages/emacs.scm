0c87150140ea264709cc1e9a4b85945e95182ded
Feng Shu <tumashu@163.com>
2022-12-30 14:06:05 +0800

    Update emacs-gee

--- gee/packages/emacs.scm
+++ gee/packages/emacs.scm
@@ -2,7 +2,10 @@
   #:use-module (gee packages)
   #:use-module (guix git-download)
   #:use-module (guix packages)
+  #:use-module (gnu packages autotools)
   #:use-module (gnu packages emacs)
+  #:use-module (gnu packages sqlite)
+  #:use-module (gnu packages text-editors)
   #:use-module (gnu packages xorg)
   #:use-module (guix gexp)
   #:use-module (guix utils)
@@ -10,26 +13,51 @@
   #:use-module (srfi srfi-9 gnu))
 
 (define-public emacs-gee
-  (set-fields
-   (package
-     (inherit emacs-next)
-     (name "emacs-gee")
-     (inputs (modify-inputs (package-inputs emacs-next)
-               (delete "gtk+")
-               (prepend libxaw)))
-     (arguments
-      (substitute-keyword-arguments (package-arguments emacs-next)
-        ((#:configure-flags flags #~'())
-         #~(cons* "--with-x-toolkit=lucid" #$flags))
-        ((#:phases phases)
-         #~(modify-phases #$phases
-             (add-after 'install 'rename-ctags
-               (lambda* (#:key outputs #:allow-other-keys)
-                 (with-directory-excursion (assoc-ref outputs "out")
-                   ;; Emacs 自带的 ctags 会和 universal-ctags 冲突，这里将其重
-                   ;; 命名。
-                   (rename-file "bin/ctags" "bin/ctags-emacs")))))))))
-   ((package-source origin-uri git-reference-url)
-    ;; Emacs git 下载速度太慢了，使用南京大学的 Emacs 镜像，同步延
-    ;; 迟大概 8 小时。https://git.savannah.gnu.org/git/emacs.git
-    "https://mirrors.nju.edu.cn/git/emacs.git")))
+  (let ((commit "0aea1cf8190aa804a0d11a67b4a3cb4b715ae82d")
+        (revision "1"))
+    (package
+      (inherit emacs)
+      (name "emacs-gee")
+      (version (git-version "29.0.50" revision commit))
+      (source
+       (origin
+         (inherit (package-source emacs))
+         (method git-fetch)
+         (uri (git-reference
+               ;; Emacs git 下载速度太慢了，使用南京大学的 Emacs 镜像，同步延
+               ;; 迟大概 8 小时。https://git.savannah.gnu.org/git/emacs.git
+               (url "https://mirrors.nju.edu.cn/git/emacs.git")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         ;; emacs-source-date-epoch.patch is no longer necessary
+         (patches (search-patches "emacs-exec-path.patch"
+                                  "emacs-fix-scheme-indent-function.patch"
+                                  "emacs-native-comp-driver-options.patch"))
+         (sha256
+          (base32
+           "17w0pabqvv2yyqfq3jbvq4cxb1yp83gvv7gws53xb4ms2mnpakwb"))))
+      (inputs
+       (modify-inputs (package-inputs emacs)
+         (delete "gtk+")
+         (prepend tree-sitter)
+         (prepend libxaw)
+         (prepend sqlite)))
+      (native-inputs
+       (modify-inputs (package-native-inputs emacs)
+         (prepend autoconf)))
+      (arguments
+       (substitute-keyword-arguments (package-arguments emacs)
+         ((#:configure-flags flags #~'())
+          #~(list "--with-modules"
+                  "--with-cairo"
+                  "--with-x-toolkit=lucid"
+                  "--with-native-compilation=no"
+                  "--disable-build-details"))
+         ((#:phases phases)
+          #~(modify-phases #$phases
+              (add-after 'install 'rename-ctags
+                (lambda* (#:key outputs #:allow-other-keys)
+                  (with-directory-excursion (assoc-ref outputs "out")
+                    ;; Emacs 自带的 ctags 会和 universal-ctags 冲突，这里将其重
+                    ;; 命名。
+                    (rename-file "bin/ctags" "bin/ctags-emacs")))))))))))
