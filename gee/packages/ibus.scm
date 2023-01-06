f4ab85f803cc18155527b2dff02764702d89efa7
Feng Shu <tumashu@163.com>
2023-01-06 11:19:31 +0800

    Update

--- gee/packages/ibus.scm
+++ gee/packages/ibus.scm
@@ -16,16 +16,65 @@
     (name "ibus-rime-gee")
     (arguments
      (substitute-keyword-arguments (package-arguments ibus-rime)
-       ;; 这里用 rime-cloverpinyin 替代 rime-data.
        ((#:configure-flags flags #~'())
         #~(list (string-append
                  "-DRIME_DATA_DIR="
-                 (assoc-ref %build-inputs "rime-cloverpinyin")
+                 (assoc-ref %build-inputs "rime-ssnhd")
                  "/share/rime-data")))))
     (inputs
      (modify-inputs (package-inputs ibus-rime)
-       (prepend rime-cloverpinyin)
+       (prepend rime-ssnhd)
        (delete "rime-data")))))
+
+(define-public rime-ssnhd
+  (let ((commit "35527a81cb6b7bb2fab1e6414c4415a5d720a3a7")
+        (revision "0"))
+    (package
+      (name "rime-ssnhd")
+      (version (git-version "0.1" revision commit))
+      (source
+       (origin
+         (uri (git-reference
+               (url "https://github.com/ssnhd/rime")
+               (commit commit)))
+         (method git-fetch)
+         (sha256
+          (base32 "0c3v7bf11fbf84cx9cxzy01wchkczpdpbyad5v4hngcz1kjfhbpk"))
+         (file-name (git-file-name name version))))
+      (build-system copy-build-system)
+      (arguments
+       (list
+        #:install-plan
+        #~'(("配置文件/" "share/rime-data/"))))
+      (home-page "https://github.com/ssnhd/rime")
+      (synopsis "Rime Settings of ssnhd.")
+      (description "Rime Settings of ssnhd (三十年河东), about 1800 stars in GitHub.")
+      (license license:lgpl2.0))))
+
+(define-public rime-wongdean
+  (let ((commit "508b0f373fd1a2475a8f531867550220193b30c5")
+        (revision "0"))
+    (package
+      (name "rime-wongdean")
+      (version (git-version "0.1" revision commit))
+      (source
+       (origin
+         (uri (git-reference
+               (url "https://github.com/wongdean/rime-settings")
+               (commit commit)))
+         (method git-fetch)
+         (sha256
+          (base32 "0rbf56icp0fr3hc0i137rv596krvqkagz9gzlw9d0vp69404a2b5"))
+         (file-name (git-file-name name version))))
+      (build-system copy-build-system)
+      (arguments
+       (list
+        #:install-plan
+        #~'(("." "share/rime-data/"))))
+      (home-page "https://github.com/wongdean/rime-settings")
+      (synopsis "Rime Settings of Wongdean.")
+      (description "Rime Settings of Wongdean (王院长), about 1300 stars in GitHub.")
+      (license license:lgpl2.0))))
 
 (define-public rime-cloverpinyin
   (package
@@ -54,54 +103,3 @@
     (synopsis "Clover Simplified pinyin input for rime")
     (description "Clover Simplified pinyin input for rime.")
     (license license:lgpl2.0)))
-
-(define-public rime-wongdean
-  (let ((commit "508b0f373fd1a2475a8f531867550220193b30c5")
-        (revision "0"))
-    (package
-      (name "rime-wongdean")
-      (version (git-version "0.1" revision commit))
-      (source
-       (origin
-         (uri (git-reference
-               (url "https://github.com/wongdean/rime-settings")
-               (commit commit)))
-         (method git-fetch)
-         (sha256
-          (base32 "0rbf56icp0fr3hc0i137rv596krvqkagz9gzlw9d0vp69404a2b5"))
-         (file-name (git-file-name name version))))
-      (build-system copy-build-system)
-      (arguments
-       (list
-        #:install-plan
-        #~'(("." "share/rime-data/"))))
-      (home-page "https://github.com/wongdean/rime-settings")
-      (synopsis "Rime Settings of Wongdean.")
-      (description "Rime Settings of Wongdean.")
-      (license license:lgpl2.0))))
-
-(define-public rime-ssnhd
-  (let ((commit "35527a81cb6b7bb2fab1e6414c4415a5d720a3a7")
-        (revision "0"))
-    (package
-      (name "rime-ssnhd")
-      (version (git-version "0.1" revision commit))
-      (source
-       (origin
-         (uri (git-reference
-               (url "https://github.com/ssnhd/rime")
-               (commit commit)))
-         (method git-fetch)
-         (sha256
-          (base32 "0c3v7bf11fbf84cx9cxzy01wchkczpdpbyad5v4hngcz1kjfhbpk"))
-         (file-name (git-file-name name version))))
-      (build-system copy-build-system)
-      (arguments
-       (list
-        #:install-plan
-        #~'(("配置文件/" "share/rime-data/"))))
-      (home-page "https://github.com/ssnhd/rime")
-      (synopsis "Rime Settings of ssnhd.")
-      (description "Rime Settings of ssnhd.")
-      (license license:lgpl2.0))))
-
