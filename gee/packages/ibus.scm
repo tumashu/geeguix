5668f79a503a02a46ed7558fb58a8483bb575c70
Feng Shu <tumashu@163.com>
2023-01-06 21:27:34 +0800

    Revert "Use ibus-libpinyin instead, remove ibus-rime"
    This reverts commit 6e15b9a12c1be0718cf05307f1a5d9e0705c803a.

---
+++ gee/packages/ibus.scm
@@ -0,0 +1,89 @@
+(define-module (gee packages ibus)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages ibus)
+  #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (guix utils)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build utils)
+  #:use-module (guix build-system copy))
+
+(define-public rime-ssnhd
+  (let ((commit "35527a81cb6b7bb2fab1e6414c4415a5d720a3a7")
+        (revision "0"))
+    (package
+      (name "rime-ssnhd")
+      (version (git-version "0.1" revision commit))
+      (source
+       (origin
+         (uri (git-reference
+               (url "https://github.com/ssnhd/rime")
+               (commit commit)))
+         (method git-fetch)
+         (sha256
+          (base32 "0c3v7bf11fbf84cx9cxzy01wchkczpdpbyad5v4hngcz1kjfhbpk"))
+         (file-name (git-file-name name version))))
+      (build-system copy-build-system)
+      (arguments
+       (list
+        #:install-plan
+        #~'(("配置文件/" "share/rime-data/"))))
+      (home-page "https://github.com/ssnhd/rime")
+      (synopsis "Rime Settings of ssnhd.")
+      (description "Rime Settings of ssnhd (三十年河东), about 1800 stars in GitHub.")
+      (license license:lgpl2.0))))
+
+(define-public rime-wongdean
+  (let ((commit "508b0f373fd1a2475a8f531867550220193b30c5")
+        (revision "0"))
+    (package
+      (name "rime-wongdean")
+      (version (git-version "0.1" revision commit))
+      (source
+       (origin
+         (uri (git-reference
+               (url "https://github.com/wongdean/rime-settings")
+               (commit commit)))
+         (method git-fetch)
+         (sha256
+          (base32 "0rbf56icp0fr3hc0i137rv596krvqkagz9gzlw9d0vp69404a2b5"))
+         (file-name (git-file-name name version))))
+      (build-system copy-build-system)
+      (arguments
+       (list
+        #:install-plan
+        #~'(("." "share/rime-data/"))))
+      (home-page "https://github.com/wongdean/rime-settings")
+      (synopsis "Rime Settings of Wongdean.")
+      (description "Rime Settings of Wongdean (王院长), about 1300 stars in GitHub.")
+      (license license:lgpl2.0))))
+
+(define-public rime-cloverpinyin
+  (package
+    (name "rime-cloverpinyin")
+    (version "1.1.4")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://github.com/fkxxyz/" name "/releases/download/"
+                           version "/clover.schema-" version ".zip"))
+       (sha256
+        (base32 "12s7zlbg2ksq4z9j4l98g00a9az0v8frdr4w35028xv7k9pnlz9j"))))
+    (build-system copy-build-system)
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (replace 'unpack
+            (lambda* (#:key source #:allow-other-keys)
+              (and (invoke "unzip" source "-d" "cloverpinyin")
+                   (chdir "cloverpinyin")))))
+      #:install-plan
+      #~'(("." "share/rime-data/"))))
+    (native-inputs (list unzip))
+    (home-page "https://github.com/fkxxyz/rime-cloverpinyin")
+    (synopsis "Clover Simplified pinyin input for rime")
+    (description "Clover Simplified pinyin input for rime.")
+    (license license:lgpl2.0)))
