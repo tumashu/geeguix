739fa413c947b448713fb1deac3e88a1dad72406
Feng Shu <tumashu@163.com>
2023-02-22 21:18:54 +0800

    Add linux-frimware-gee and use it

--- gee/packages/linux.scm
+++ gee/packages/linux.scm
@@ -1,6 +1,7 @@
 (define-module (gee packages linux)
   #:use-module (gnu)
   #:use-module (gnu packages linux)
+  #:use-module (guix download)
   #:use-module (guix packages)
   #:use-module (nongnu packages linux))
 
@@ -11,3 +12,16 @@
    #:configs
    '("# Add by linux-thinkpad-t14-amd."
      "CONFIG_MT7921E=m")))
+
+(define-public linux-firmware-gee
+  (package
+    (inherit linux-firmware)
+    (name "linux-firmware-gee")
+    (version "20221214")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://mirrors.nju.edu.cn/kernel/firmware/"
+                                  "linux-firmware-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1f93aq0a35niv8qv8wyy033palpplbgr2cq0vihb97wxfkk5wmr2"))))))
